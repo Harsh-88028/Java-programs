7ef196cbe1054d0b8bc721e7ea1ddd49bd5633f4
Harsh-88028 <81902272+Harsh-88028@users.noreply.github.com>
2021-06-10 11:16:12 +0530

    Create Q7(Practical).java

---
+++ Q7(Practical).java
@@ -0,0 +1,35 @@
+7. Write a program to read a file and display only those lines that have the first two characters as '//' (Use try with resources).
+
+
+package com.journaldev.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+
+public class Java7ResourceManagement {
+
+	public static void main(String[] args) {
+
+		try (MyResource mr = new MyResource()) {
+			System.out.println("MyResource created in try-with-resources");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Out of try-catch block.");
+	}
+
+	static class MyResource implements AutoCloseable{
+
+		@Override
+		public void close() throws Exception {
+			System.out.println("Closing MyResource");
+		}
+
+	}
+}
+
+Output -
+  
