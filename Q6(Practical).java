430ea8aeac641875b26bc3a7f203e9e64f1f986d
Harsh-88028 <81902272+Harsh-88028@users.noreply.github.com>
2021-06-10 11:03:05 +0530

    Create Q6(Practical).java

---
+++ Q6(Practical).java
@@ -0,0 +1,37 @@
+6.Write a program that copies content of one file to another. Pass the names of the files through command-line arguments.
+  
+
+/**** Copy.java ****/
+import java.io.*;
+public class Copy {
+public static void main(String[] args) throws Exception {
+if (args.length != 2) {
+System.err.println("Usage: java Copy <src> <dest>");
+} else {
+int i;
+FileInputStream fin = new FileInputStream(args[0]);
+FileOutputStream fout = new FileOutputStream(args[1]);
+while ((i = fin.read()) != -1) {
+fout.write(i);
+}
+fin.close();
+fout.close();
+System.out.println(
+"Copied contents of" + args[0] + " to " + args[1]
+);
+}
+}
+}
+
+Output -
+  
+> java Copy src.txt dest.txt
+Copied contents of src.txt to dest.txt
+> type .\src.txt
+Harsh Meena
+===========
+University of Delhi
+> type .\dest.txt
+Harsh Meena
+===========
+University of Delhi
